193383248530eb33a456e377c13aec7857ca6c2c
wucong <q1875486458@gmail.com>
2022-07-31 23:44:20 +0800

    update

--- actions/stapxx.actions.sh
+++ actions/stapxx.actions.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
+pid="37745"
 
+# bash -x -c '. ./actions/stapxx.actions.sh;stapxx-run'
 function stapxx-run () {
     sudo rm -rf ./stapxx-*
-    sudo ./stap++ -v ./samples/lj-lua-stacks.sxx -x 2887528 -D MAXSKIPPED=200000 --arg time=50
+    local sxx="./wg-samples/5-luajit-backtrace-stack.sxx"
+    local time=300
+    local flag=" -v -D MAXMAPENTRIES=200000  -D MAXSKIPPED=200000 -x $pid  --arg time=$time"
+    rm -rf ./out
+    mkdir -p ./out
+    sudo ./stap++ $sxx  $flag |tee ./out/stap.log
     sudo chmod a+r ./stapxx-* 
     sudo chmod -R 755 $(ls |grep stapxx-)
+    stapxx-svg ./out/stap.log
+    firefox ./out/stacks.svg &!
 }
-# sudo rm -rf ./stapxx-* ;sudo ./stap++   -v ./wg-samples/3-luajit-backtrace.sxx  -D MAXSKIPPED=200000  -x 2887528  --arg time=1 |tee stap.log
+
+function stapxx-svg () {
+    local log=$1
+    cat $log |grep -a 'report:' |sed 's/report://g' > ./out/stacks.bt
+    sed -i 's/|lb|/\n/g' ./out/stacks.bt
+    sed -i 's/|stack-fs|/\n/g' ./out/stacks.bt
+    sed -i 's/|stack-fe|//g' ./out/stacks.bt
+    sed -i 's/|fe|/\n/g' ./out/stacks.bt
+    sed -i 's/|pf-s|//g' ./out/stacks.bt
+    sed -i 's/|pf-e|//g' ./out/stacks.bt
+    sed -i 's/|vm_c|//g' ./out/stacks.bt
+    sed -i 's/|trace-f|//g' ./out/stacks.bt
+    sed -i 's/|stack|//g' ./out/stacks.bt
+    sed -i 's/|lt|/\n\t/g' ./out/stacks.bt
+    ./actions/fix-lua-bt ./out/stacks.bt > ./out/stacks.fix.bt
+    ./actions/stackcollapse-stap.pl  ./out/stacks.fix.bt >  ./out/stacks.cbt
+    ./actions/flamegraph.pl --encoding="ISO-8859-1" \
+              --title="Lua-land on-CPU flamegraph" \
+              ./out/stacks.cbt > ./out/stacks.svg
+}
